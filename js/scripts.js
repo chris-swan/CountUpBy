dda65ba89f270c9897544295a8b3503f8827fe5c
steve smietana & chris swan <student@epicodus.com>
2015-09-10 10:18:45 -0700

    fixed html

--- js/scripts.js
+++ js/scripts.js
@@ -2,31 +2,25 @@ var countBy = function(total, multiple) {
 
     var result = [];
 
-    for (var i = 1; i*multiple <= total; i++) {
-        result.push(i*multiple);
+    if(total < multiple) {
+        alert("Please enter a multiple lower than the total");
+        document.write("Please try again with a lower count by value!");
+    } else {
+        for (var i = 1; i*multiple <= total; i++) {
+            result.push(i*multiple);
+        }
+        return result;
     }
-    return result;
 }
 
-// $(document).ready(function() {
-//
-//     $("form#prime").submit(function(event) {
-//
-//         var input = $("input").val()
-//         var result = prime(input)
-//
-//         $(".phrase").text(result)
-//         $("#result").show()
-//         debugger
-//         event.preventDefault()
-//     });
-// });
+    $(document).ready(function() {
+      $("form#countBy").submit(function(event) {
+        var total = $("input#total").val();
+        var multiple= $("input#multiple").val();
+        var result = countBy(total, multiple);
+        $(".result").append("<li>" + String(result) + "</li>");
 
-// total(10) multiple(2)
-//
-// 2xi
-// i=1
-//
-// i+1 <= total
-//
-// array (2xi, 2xi+1, 2*i+1+1... 10)
+        event.preventDefault();
+     });
+
+});
